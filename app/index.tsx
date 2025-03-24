5904d3fb6de2d9799bb223377f6d6c62ab0e7ed6
Kennedy2042 <ogbonnakenedy@gmail.com>
2025-03-24 16:17:31 +0100

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import HomeScreen from "../screens/HomeScreen"
+
+export default function Index() {
+  return (
+      <HomeScreen/>
+    
+  );
+}
---
+++ components/categories.js
@@ -0,0 +1,32 @@
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { categories } from '../constants'
+
+export default function Categories() {
+    return (
+        <View className='mt-4 '>
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                className='overflow-visible'
+                contentContainerStyle={{
+                    paddingHorizontal: 15
+                }}
+            >
+                {
+                    categories.map((category, index) => {
+                        return (
+                            <View key={index} className='flex justify-center items-center mr-6'>
+                                <TouchableOpacity 
+                                className='p-1 rounded-full bg-gray-200'>
+                                    <Image style={{width: 45, height: 45}} source={category.image}/>
+                                    <Text className='text-sm'>{category.name}</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )
+                    })
+                }
+            </ScrollView>
+        </View>
+    )
+}
